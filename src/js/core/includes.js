9f1e51f9a78916c55d818026148aa1df276e50cc
Henrique-Aguiar <hrikelr@hotmail.com>
2022-05-09 22:59:59 -0300

    botoes e rodapé

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -1,5 +1,13 @@
 import $, { param } from 'jquery'
 
+const loadHtmlSuccessCallbacks = []
+
+export function onLoadHtmlSuccess(callback) {
+  if(!loadHtmlSuccessCallbacks.includes(callback)) {
+    loadHtmlSuccessCallbacks.push(callback)
+  }
+}
+
 function loadIncloudes(parent) {
   if(!parent) parent = 'body'
   $(parent).find('[wm-include]').each(function(i, e) {
@@ -10,6 +18,7 @@ function loadIncloudes(parent) {
         $(e).html(data)
         $(e).removeAttr('wm-include')
         
+        loadHtmlSuccessCallbacks.forEach(callback => callback(data))
         loadIncloudes(e)
       }
     })
